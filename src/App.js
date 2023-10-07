6b32be75b89e5bd6579b23460b9a8de06c6e6083
Rahat Ho <rahat.ho@gmail.com>
2023-10-07 15:02:20 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import {useState} from 'react';
 
 function App() {
-
+  const [outputText, setOutputText] = useState('Initial text');
+  function updateTextHandler(){
+    setOutputText('Text was changed');
+  }
   return (
-    
+    <>
+      <button onClick={updateTextHandler}>Click to change text</button>
+      <p>{outputText}</p>
+    </>
   );
 }
 
