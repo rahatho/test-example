ba278a45dea1428a4d716f5bf7da47d0d1b02a34
Rahat Ho <rahat.ho@gmail.com>
2023-10-08 16:21:49 +0600

    object destructuring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 
 
 
-function Greeting(){
-  return (<p> Welcome to this book! </p>) ;
+function GoalItem(props){
+  return <li>{props.children} (ID: {props.id})</li> ;
 };
 
 function App() {
- 
+ const user = {name: 'Rahat', age: 32};
+ const {name, age} = user; //object destructuring
+ console.log(name);
   
   return (
     <div>
       <h2>Hello World!</h2>
-      <Greeting/>
+      <ul>
+        <GoalItem id='1'>Read</GoalItem>
+        <GoalItem  id="2">Learn</GoalItem>
+      </ul>
     </div>
   );
   };
