52bc5caafce825736a4163c5c625a0251b9d12ae
Rahat Ho <rahat.ho@gmail.com>
2023-10-13 13:54:16 +0600

    react-without-js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ function GoalItem(props){
 };
 
 function App() {
- const user = {name: 'Rahat', age: 32};
+ const user = {name: 'Max', age: 32};
  const {name, age} = user; //object destructuring
  console.log(name);
-  
+
+ function Link({config, children}){
+  return <a {...config} target="_blank" rel="noopener noreferrer">{children}</a>;
+ };
+  const config = {href:'https://www.google.com'};
   return (
     <div>
       <h2>Hello World!</h2>
       <ul>
         <GoalItem id='1'>Read</GoalItem>
         <GoalItem  id="2">Learn</GoalItem>
+        <Link config={config}>Click link</Link>
       </ul>
     </div>
   );
